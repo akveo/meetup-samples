f868178aad576306b1e3d9603c5aea0461134ad6
Yauhen <e.penkin@akveo.com>
2018-12-03 13:28:53 +0300

    build(app): es-6 symbol dependency add

--- DevFestConf.Dec2018.RN.Mobx.Sample/index.js
+++ DevFestConf.Dec2018.RN.Mobx.Sample/index.js
@@ -1,3 +1,4 @@
+import 'es6-symbol/implement';
 import { AppRegistry, YellowBox } from 'react-native';
 import { name as appName } from './app.json';
 import { AppContainer } from './app/app.container';
